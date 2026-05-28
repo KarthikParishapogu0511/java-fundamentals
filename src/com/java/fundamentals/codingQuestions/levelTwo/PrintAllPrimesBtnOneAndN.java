511716bf34226383d036ba0d46983f883b184081
KarthikParishapogu0511 <karthikparishapogu0511@gmail.com>
2026-05-28 15:22:45 +0530

    Print all primes between 1 and n

--- src/com/java/fundamentals/codingQuestions/levelTwo/PrintAllPrimesBtnOneAndN.java
+++ src/com/java/fundamentals/codingQuestions/levelTwo/PrintAllPrimesBtnOneAndN.java
@@ -1,4 +1,26 @@
 package com.java.fundamentals.codingQuestions.levelTwo;
 
+import java.util.Scanner;
+
 public class PrintAllPrimesBtnOneAndN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Number :");
+        int n = scanner.nextInt();
+        boolean isPrime;
+        if(n <= 1) isPrime = false;
+        for(int i =2;i <= n;i++){
+            isPrime =true;
+            for(int j = 2;j <= i/2 ;j++){
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.print(i + " ");
+            }
+
+        }
+    }
 }
