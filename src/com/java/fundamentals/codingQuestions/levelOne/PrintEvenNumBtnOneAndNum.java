a8c6d1127c04d8724dcba28917ff9253b80b6efc
KarthikParishapogu0511 <karthikparishapogu0511@gmail.com>
2026-05-28 12:52:11 +0530

    Print all Even Nums from 1 to N in all 3 loops

---
+++ src/com/java/fundamentals/codingQuestions/levelOne/PrintEvenNumBtnOneAndNum.java
@@ -0,0 +1,34 @@
+package com.java.fundamentals.codingQuestions.levelOne;
+
+import java.util.Scanner;
+
+public class PrintEvenNumBtnOneAndNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Num : ");
+        int n = scanner.nextInt();
+        System.out.println("Using For Loop");
+        for(int i = 1;i <= n;i++){
+            if(i % 2 == 0) System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("------------");
+
+        System.out.println("Using While Loop Logic");
+
+        int j = 1;
+        while(j <= n){
+            if(j % 2 == 0) System.out.print(j + " ");
+            j++;
+        }
+        System.out.println();
+        System.out.println("---------------------");
+
+        System.out.println("Using Do While Loop Logic");
+        int k = 1;
+        do{
+            if(k % 2== 0) System.out.print(k + " ");
+            k++;
+        }while(k <= n);
+    }
+}
