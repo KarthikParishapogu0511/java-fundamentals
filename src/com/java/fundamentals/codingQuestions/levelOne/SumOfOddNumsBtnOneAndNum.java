413fbd4cc3e58daef0a4c7ee42d53003ceaf2c80
KarthikParishapogu0511 <karthikparishapogu0511@gmail.com>
2026-05-31 16:08:24 +0530

    feat : Print All Odd Nums btn One and Num

--- src/com/java/fundamentals/codingQuestions/levelOne/SumOfOddNumsBtnOneAndNum.java
+++ src/com/java/fundamentals/codingQuestions/levelOne/SumOfOddNumsBtnOneAndNum.java
@@ -1,4 +1,37 @@
 package com.java.fundamentals.codingQuestions.levelOne;
 
+import java.util.Scanner;
+
 public class SumOfOddNumsBtnOneAndNum {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int n = scanner.nextInt();
+        //using for loop
+        int sumfor = 0;
+        for(int i = 1;i <=n;i++){
+            if(i % 2 != 0)sumfor += i;
+        }
+        System.out.println("using For loop sum of Odd Nums :" + sumfor);
+
+        System.out.println("-----------------------");
+        //using do-while loop
+        int j =1;
+        int sumDoWhile = 0;
+        do{
+            if(j % 2 != 0) sumDoWhile += j;
+            j++;
+        }while(j<=n);
+        System.out.println("Using Do While loop : " + sumDoWhile);
+        System.out.println("-------------------------");
+
+        // using while loop
+        int k =1;
+        int sumWhile = 0;
+        while(k <= n){
+            if(k % 2 != 0) sumWhile += k;
+            k++;
+        }
+        System.out.println("Using While Loop : " + sumWhile) ;
+    }
 }
