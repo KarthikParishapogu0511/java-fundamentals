7deb1bb38922353775fbb3ee1d7098fef68d6c06
KarthikParishapogu0511 <karthikparishapogu0511@gmail.com>
2026-06-27 19:25:04 +0530

    feat: is Harshad Niven Number

---
+++ src/com/java/fundamentals/codingQuestions/levelThree/isHarshadNivenNumber.java
@@ -0,0 +1,23 @@
+package com.java.fundamentals.codingQuestions.levelThree;
+
+import java.util.Scanner;
+
+public class isHarshadNivenNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = input.nextInt();
+        int temp = n;
+        int sum =0;
+        while (temp > 0){
+            int digit = temp % 10;
+            sum += digit;
+            temp /= 10;
+        }
+        if(n % sum == 0){
+            System.out.println("It's Harshad Niven Number");
+        }else {
+            System.out.println("Not Harshad Niven Number");
+        }
+    }
+}
