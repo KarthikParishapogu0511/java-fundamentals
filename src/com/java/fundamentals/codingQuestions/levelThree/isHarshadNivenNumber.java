2761820e9ef4caffaf16f0b11b5c397668572a64
KarthikParishapogu0511 <karthikparishapogu0511@gmail.com>
2026-06-27 19:28:16 +0530

    feat: harshad number comment

--- src/com/java/fundamentals/codingQuestions/levelThree/isHarshadNivenNumber.java
+++ src/com/java/fundamentals/codingQuestions/levelThree/isHarshadNivenNumber.java
@@ -3,7 +3,10 @@ package com.java.fundamentals.codingQuestions.levelThree;
 import java.util.Scanner;
 
 public class isHarshadNivenNumber {
+
     public static void main(String[] args) {
+
+        // A number is called harshad Niven number when the number itself is divisible by the sum of its own digits
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int n = input.nextInt();
